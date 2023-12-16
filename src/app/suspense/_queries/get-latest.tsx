f89eb2432bced80c44cf47dbac600e21bba9d492
Ivan B <sensenabi@gmail.com>
2023-12-15 19:37:32 -0500

    suspense

---
+++ src/app/suspense/_queries/get-latest.tsx
@@ -0,0 +1,17 @@
+import { api } from "@/trpc/server";
+
+export async function GetLatest() {
+  const start = Date.now();
+  const latestPost = await api.post.getLatest.query();
+  const duration = Date.now() - start;
+  return (
+    <>
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+      Drizzle + PlanetScale (us-east-1 Virginia) {duration}ms
+    </>
+  );
+}
